a03e6e64a60f2b549b1d1b652524c8b20b12d975
Izzatullo21092003 <91390212+Izzatullo21092003@users.noreply.github.com>
2022-10-20 20:32:34 +0500

    To Do List App

---
+++ to_do_project/todo_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from todo_app import views
+
+urlpatterns = [
+    path("", views.List_View.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
+    path("list/add/", views.ListCreate.as_view(), name="list-add"),
+    path("list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"),
+    path("list/<int:list_id>/item/add/", views.ItemCreate.as_view(), name="item-add",),
+    path("list/<int:list_id>/item/<int:pk>/", views.ItemUpdate.as_view(), name="item-update",),
+    path("list/<int:list_id>/item/<int:pk>/delete/", views.ItemDelete.as_view(), name="item-delete",),
+]
